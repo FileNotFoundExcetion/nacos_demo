aa321b6e00a0438339927bc2ecde4075bfffb132
yunfeng <yunfeng@payworth.cn>
2023-07-24 14:32:23 +0800

    feat:add nacos demo

--- src/main/java/com/sharding/controller/TestController.java
+++ src/main/java/com/sharding/controller/TestController.java
@@ -1,26 +1,29 @@
 package com.sharding.controller;
 
-import cn.hutool.core.io.IoUtil;
+import com.sharding.config.ShengTengProperties;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletInputStream;
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.Map;
 
 @RestController
 public class TestController {
+    @Resource
+    private ShengTengProperties shengTengProperties;
     @PostMapping("hello")
     public Object ss(HttpSession httpSession, HttpServletRequest request) throws IOException {
-        String parameter = request.getParameter("p");
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        ServletInputStream inputStream = request.getInputStream();
-        System.out.println(IoUtil.read(inputStream, Charset.defaultCharset()));
-        System.out.println(parameterMap);
-        System.out.println(parameter);
+//        String parameter = request.getParameter("p");
+//        Map<String, String[]> parameterMap = request.getParameterMap();
+//        ServletInputStream inputStream = request.getInputStream();
+//        System.out.println(IoUtil.read(inputStream, Charset.defaultCharset()));
+//        System.out.println(parameterMap);
+//        System.out.println(parameter);
+        System.out.println("--------------");
+        System.out.println(shengTengProperties.getPtid());
+        System.out.println("--------------");
         return httpSession.getId();
     }
 
