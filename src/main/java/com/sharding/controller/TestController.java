b1c8301aa7fe15ccd589982cf7436d799c8b0793
yunfeng <yunfeng@payworth.cn>
2023-07-27 20:10:14 +0800

    feat:add query

--- src/main/java/com/sharding/controller/TestController.java
+++ src/main/java/com/sharding/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.sharding.controller;
 
 import com.sharding.config.ShengTengProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@RefreshScope
 @RestController
 public class TestController {
     @Resource
     private ShengTengProperties shengTengProperties;
 
+    @Value("${shengteng.ptid}")
+    private String ptid;
+
     @PostMapping("hello")
     public Object ss(HttpSession httpSession, HttpServletRequest request) throws IOException {
         System.out.println("--------------");
-        System.out.println(shengTengProperties.getPtid());
+        System.out.println(ptid);
         System.out.println("--------------");
         return httpSession.getId();
     }
